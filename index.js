f82c44e40d5cf294ce988e6979bb6c2b11cc72b9
Leandro <desenvolvedor02@adias.com.br>
2018-12-21 09:28:11 -0200

    Nova API e melhorando a view.

--- index.js
+++ index.js
@@ -24,8 +24,14 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+const HGWeatherURL = 'https://api.hgbrasil.com/weather/';
+const HGWeatherKey = '948f7313';
+
+const OpenWeatherMapURL = 'http://api.openweathermap.org/data/2.5/weather';
+const OpenWeatherMapURLKey = '1d45b5f14ba9cbc264e3c2b018b6bdfd';
+
 app.post('/', (req, res) => {
-    axios.get(`https://api.hgbrasil.com/weather/?format=json&city_name=${encodeURI(req.body.city)}&key=948f7313`)
+    axios.get(`${HGWeatherURL}?format=json&city_name=${encodeURI(req.body.city)}&key=${HGWeatherKey}`)
         .then((result)=> {
             res.render('index', { 
                     success : true,
@@ -34,9 +40,15 @@ app.post('/', (req, res) => {
                 }
             );
         }
-    ).catch((erro) => {
-        res.send('deu erro' + erro);
+    ).then((result) => {
+        res.send('olá');
+    }).catch((erro) => {
+        res.render('index', {
+            erro : true
+        });
     });
 });
 
 app.listen('3000');
+
+// ?q=Bahia,BR&appid=&units=Metric
